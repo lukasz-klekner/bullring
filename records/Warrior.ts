a9eb2fecb148aeff42c1a9653d1e92d582b4843e
Luki Klekner <lukaszklekner@gmail.com>
2023-02-23 16:48:45 +0100

    feat: add insert method and enhance validation

--- records/Warrior.ts
+++ records/Warrior.ts
@@ -1,7 +1,19 @@
+import { ObjectId } from "mongodb";
+import { warriorCollection } from "../utils/db";
 import { ValidationError } from "../utils/errors";
 
+interface WarriorSchema {
+    _id?: ObjectId;
+    readonly name: string;
+    readonly power: number;
+    readonly defence: number;
+    readonly strength: number;
+    readonly agility: number;
+    wins?: number;   
+}
+
 export class WarriorRecord {
-    _id?: string;
+    _id?: ObjectId;
     readonly name: string;
     readonly power: number;
     readonly defence: number;
@@ -9,11 +21,19 @@ export class WarriorRecord {
     readonly agility: number;
     wins?: number;
 
-    constructor(object: WarriorRecord){
+    constructor(object: WarriorSchema){
         const { _id, name, power, defence, strength, agility, wins } = object;
 
-        const sum = [power, defence, strength, agility].reduce((prev,current) => prev+current, 0) 
+        const stats = [power, defence, strength, agility]
 
+        for(const stat of stats){
+            if(stat < 1){
+                throw new ValidationError('Any skill needs to be grater than 1.') 
+            }
+        }
+
+        const sum = stats.reduce((prev,current) => prev+current, 0)
+        
         if(sum !== 10) {
             throw new ValidationError('Sum of all skills need to be equal 10.')
         }
@@ -28,6 +48,13 @@ export class WarriorRecord {
         this.defence = defence
         this.strength = strength
         this.agility = agility
-        this.wins = wins
+        this.wins = wins ?? 0
     }
+
+    async insert(){
+        await warriorCollection.insertOne(this)
+
+        return this._id.toString()
+    } 
+
 }

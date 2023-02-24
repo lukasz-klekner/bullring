3b528aec316d6eb6bd362d008530f79660861571
Luki Klekner <lukaszklekner@gmail.com>
2023-02-24 15:40:51 +0100

    feat: add fullring form

--- routers/bullring.ts
+++ routers/bullring.ts
@@ -1,11 +1,34 @@
 import { Router } from "express";
+import { WarriorRecord } from "../records/Warrior";
+import { ValidationError } from "../utils/errors";
 
 export const bullringRouter = Router()
 
 bullringRouter
-    .get('/fight-form', (_, res) => {
-        res.render('bullring/fight-form')
+    .get('/fight-form', async (_, res) => {
+        const warriors = await WarriorRecord.listAll()
+
+        res.render('bullring/fight-form', {
+            warriors
+        })
     })
-    .post('/fight', (req, res) => {
-        res.render('bullring/fight')
+    .post('/fight', async (req, res) => {
+        const { warrior1: warrior1Id, warrior2: warrior2Id } = req.body
+
+        if(warrior1Id === warrior2Id){
+            throw new ValidationError('The same warrior! Change one of them to start a battle')
+        }
+
+        const warrior1 = await WarriorRecord.findOne(warrior1Id)
+        const warrior2 = await WarriorRecord.findOne(warrior2Id)
+
+        if(warrior1){
+            throw new ValidationError('The warrior1 does not exist')
+        }
+
+        if(warrior2){
+            throw new ValidationError('The warrior2 does not exist')
+        }
+
+         res.render('bullring/fight')
     })

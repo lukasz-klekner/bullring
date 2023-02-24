24b4132544cd96a50dbcfe9350cef5a12fac5146
Luki Klekner <lukaszklekner@gmail.com>
2023-02-24 14:21:07 +0100

    feat: add list the hall of fame

--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import { WarriorRecord } from "../records/Warrior";
 
 export const hallOfFameRouter = Router()
 
-hallOfFameRouter.get('/', (_, res) => {
-    res.render('hall-of-fame/list')
+hallOfFameRouter.get('/', async (_, res) => {
+    const warriors = (await WarriorRecord.listTop(10)).map((warrior, index) => ({
+        place: index+1,
+        ...warrior
+    }))
+
+    res.render('hall-of-fame/list', {
+        warriors
+    })
 })
